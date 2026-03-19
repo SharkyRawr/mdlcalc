e81d67d2bf303c398cbb47b0c263e3afb5e5c5d0
Luna <me@luna.vg>
2026-03-19 21:12:58 +0100

    Add GitHub Actions workflows for building and publishing Homebrew bottles

--- Formula/mdlcalc.rb
+++ Formula/mdlcalc.rb
@@ -3,15 +3,19 @@ class Mdlcalc < Formula
 
   desc "Calculate approximate memory requirements for LLM inference"
   homepage "https://github.com/SharkyRawr/mdlcalc"
-  url "https://github.com/SharkyRawr/mdlcalc/releases/download/v0.1.1/mdlcalc-0.1.0-py3-none-any.whl"
-  sha256 "91e822fbd3291cd60fdd5c12376022e6eb6e3b2d05e6d2d8a98f24ccd778a233"
+  url "https://github.com/SharkyRawr/mdlcalc/releases/download/v0.1.1/mdlcalc-0.1.0.tar.gz"
+  sha256 "9504f58407b8083107d9dbfb7a339549ce1adb30ec3d5e034b3ecd84012d0654"
   license "CC-BY-NC-SA-4.0"
 
   depends_on "python@3.12"
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/3d/fa/656b739db8587d7b5dfa22e22ed02566950fbfbcdc20311993483657a5c0/click-8.3.1.tar.gz"
+    sha256 "12ff4785d337a1bb490bb7e9c2b1ee5da3112e94a8622f26a6c77f5d2fc6842a"
+  end
+
   def install
-    venv = virtualenv_create(libexec, "python3.12")
-    venv.pip_install_and_link buildpath
+    virtualenv_install_with_resources
   end
 
   test do
